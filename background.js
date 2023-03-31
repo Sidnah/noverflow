c14f1fa7126e1dcce7b852cbc359034fbf3c521d
Manu Misson <manu@lesmanifestes.com>
2023-03-30 20:44:26 -0400

    set icon and add badge on action

--- background.js
+++ background.js
@@ -1,6 +1,18 @@
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
-    files: ["scripts/noverflow.js"],
-    target: { tabId: tab.id }
+    target: { tabId: tab.id },
+    files: ["scripts/noverflow.js"]
+  });
+  chrome.action.setIcon({
+    tabId: tab.id,
+    path: 'images/active.png'
+  });
+  chrome.action.setBadgeBackgroundColor({
+    tabId: tab.id,
+    color: '#71d163'
+  });
+  chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: '✓'
   });
 });
