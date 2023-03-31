f255e5b1e7e47f3d256bcf425377faa778c8a41f
Manu Misson <manu@lesmanifestes.com>
2023-03-30 20:20:33 -0400

    fix style overriding itself and extension crash if already clicked becaus eof const

--- scripts/noverflow.js
+++ scripts/noverflow.js
@@ -1,10 +1,3 @@
-const body = document.querySelector('body');
-const html = document.querySelector('html');
 
-body.setAttribute('style', 'overflow:auto!important');
-body.setAttribute('style', 'height:auto!important');
-body.setAttribute('style', 'max-height:auto!important');
-
-html.setAttribute('style', 'overflow:auto!important');
-html.setAttribute('style', 'height:auto!important');
-html.setAttribute('style', 'max-height:auto!important');
+document.querySelector('body').setAttribute('style', 'overflow:auto!important;height:auto!important;max-height:none!important');
+document.querySelector('html').setAttribute('style', 'overflow:auto!important;height:auto!important;max-height:none!important');
